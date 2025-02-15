b5d930a996a7926dab30c0b30c972c3fab763996
anushkach14 <129940327+anushkach14@users.noreply.github.com>
2025-02-14 22:11:40 -0800

    Delete w4q3.py

--- w4q3.py
+++
@@ -1,6 +0,0 @@
-def circle_area(r):
-    return 3.14159 * r**2
-
-radius = float(input("Enter the radius of the circle: "))
-area = circle_area(radius)
-print(f"The area of the circle with radius {radius} is: {area:.3f}")
