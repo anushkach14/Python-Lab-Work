7d7ec8c2bae5e67e9c9e428731d1eecfcc19f0e1
anushkach14 <129940327+anushkach14@users.noreply.github.com>
2025-02-14 22:09:54 -0800

    Delete w3q11.py

--- w3q11.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter a number: "))
-a, b = 0, 1
-while a < n:
-    a, b = b, a + b
-if a == n:
-    print(f"{n} is a Fibonacci number.")
-else:
-    print(f"{n} is not a Fibonacci number.")
