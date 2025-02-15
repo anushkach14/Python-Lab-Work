ee8a6f60fc93069d3d91de2f5ed57aa0e7065c38
anushkach14 <129940327+anushkach14@users.noreply.github.com>
2025-02-14 22:12:50 -0800

    Delete w5q10.py

--- w5q10.py
+++
@@ -1,9 +0,0 @@
-s = input("Enter string: ")
-vowels = "aeiouAEIOU"
-counts = {}
-
-for char in s:
-    if char in vowels:
-        counts[char] = counts.get(char, 0) + 1  # Update the count
-
-print(counts)
