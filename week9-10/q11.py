8182814c247c8ec00287bd4e913bab441346f934
anushkach14 <129940327+anushkach14@users.noreply.github.com>
2025-04-20 22:34:57 +0530

    Update q11.py

--- week9-10/q11.py
+++ week9-10/q11.py
@@ -1,19 +1,10 @@
-# Function to implement bubble sort
 def bubble_sort(arr):
-    # Traverse through all elements in the list
-    for i in range(len(arr)):
-        # Compare adjacent elements
-        for j in range(0, len(arr) - i - 1):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
-                # Swap if elements are in the wrong order
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # Swap
 
-# Input: Taking a list of numbers from the user
-user_input = input("Enter numbers separated by spaces: ")
-arr = [int(x) for x in user_input.split()]
-
-# Sorting the list using bubble sort
+arr = list(map(int, input("Enter numbers separated by spaces:").split()))
 bubble_sort(arr)
-
-# Displaying the sorted list
-print("Sorted list:", arr)
+print(arr)
