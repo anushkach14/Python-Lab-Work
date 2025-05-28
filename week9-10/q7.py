b8bb8378f4d0a30ae69c503ab0e20127ebe11045
anushkach14 <129940327+anushkach14@users.noreply.github.com>
2025-05-28 15:38:25 +0530

    Update q7.py

--- week9-10/q7.py
+++ week9-10/q7.py
@@ -1,11 +1,3 @@
-# Taking input from the user as a space-separated string of numbers
-user_input = input("Enter elements separated by spaces: ")
-
-# Converting the input string into a list of integers
-mylist = [int(x) for x in user_input.split()]
-
-# Sorting the list in ascending order
-mylist.sort()
-
-# Displaying the sorted list
-print(f"The sorted list is: {mylist}")
+n=[int(x) for x in input("Enter elements: ").split()]
+n.sort()
+print(f"The sorted list is: {n}")
